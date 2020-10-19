9d400ad08b398384ff98a5c72828845da450098d
HansMandt <HansMandt98@gmail.com>
2020-10-19 07:26:02 -0700

    Added a second activity and set up a basic login system

--- app/src/main/java/com/example/environmentsetup/MainActivity.java
+++ app/src/main/java/com/example/environmentsetup/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.environmentsetup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,10 +20,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sayHello(View view) {
-        Log.i("INFO", "Hello Android!");
-        TextView tv = findViewById(R.id.FirstNameLastName);
-        tv.setText("Hello Android!");
+    public void LogIn(View view) {
+
+        Log.i("INFO", "Logging into your account...");
+        EditText user = findViewById(R.id.userNameInput);
+        EditText password = findViewById(R.id.passwordInput);
+
+        String userNameInput = user.getText().toString();
+        String passwordInput = password.getText().toString();
+
+        Log.i("INFO", "The username you input is " + user.getText().toString());
+        Log.i("INFO", "The password you input is " + password.getText().toString());
+
+        Intent intent = new Intent(getApplicationContext(), UserConfirmationActivity.class);
+        intent.putExtra("usernameCredential", userNameInput);
+        intent.putExtra("passwordCredential", passwordInput);
+        startActivity(intent);
     }
+
 
 }
--- app/src/main/java/com/example/environmentsetup/UserConfirmationActivity.java
+++ app/src/main/java/com/example/environmentsetup/UserConfirmationActivity.java
@@ -3,12 +3,38 @@ package com.example.environmentsetup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class UserConfirmationActivity extends AppCompatActivity {
+
+    private String storedUsername = "cleverUsername";
+    private String storedPassword = "123456789";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_confirmation);
+
+        TextView tv = findViewById(R.id.textView);
+        String usernameInput = getIntent().getStringExtra("usernameCredential");
+        String passwordInput = getIntent().getStringExtra("passwordCredential");
+
+        if(usernameInput.equals(storedUsername) && passwordInput.equals(storedPassword))
+        {
+            tv.setText("Welcome, " + storedUsername + "!");
+        }
+        else
+        {
+            tv.setText("Username/Password combination is wrong");
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("INFO", "UserConformationActivity is destroyed");
     }
 }
